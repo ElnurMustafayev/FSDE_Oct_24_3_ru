9ae73e748b9c3f21b68cae3d67f886dcb5a6fdcd
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-05-16 21:52:31 +0400

    Add C# basics

--- C#/GettingStartedApp/Program.cs
+++ C#/GettingStartedApp/Program.cs
@@ -1,11 +1,55 @@
-﻿void MyFunc()
+﻿/*
+Console.Write("Input your age: ");
+string userAgeInput = Console.ReadLine();
+
+int userAge = Convert.ToInt32(userAgeInput);
+
+bool isAdult = userAge >= 18;
+
+if(isAdult)
 {
-    int num = 1;
-    num++;
-    Console.WriteLine("FUNC");
-    Console.WriteLine(num);
+    Console.WriteLine("You are adult!");
+}
+else
+{
+    Console.WriteLine("You are not adult!");
+}
+*/
+
+
+
+/*
+int counter = 0;
+
+while(counter < 10)
+{
+    if (counter == 3)
+        //continue; 
+        break; 
+
+    counter++;
+
+    
+    Console.WriteLine(counter);
+}
+*/
+
+bool IterationMethod(string text)
+{
+    Console.WriteLine(text);
+
+    return true;
 }
 
-MyFunc();
-MyFunc();
-MyFunc();
+
+// ABDCBDCBDCBDCBDC
+for (Console.WriteLine("A"); IterationMethod("B"); Console.WriteLine("C"))
+{
+    Console.WriteLine("D");
+}
+
+
+//for (CreateUser(); CheckUserInfo(); GetNextUser())
+//{
+//    PrintUserInfo();
+//}
