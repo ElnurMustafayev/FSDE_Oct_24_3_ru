ab75013e939be1a7dadb35e8984fcd8737c7c3da
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-05-23 21:47:16 +0400

    Add Arrays

---
+++ C#/ArraysApp/Program.cs
@@ -0,0 +1,72 @@
+﻿//int[] numbers = new int[3];
+//numbers[1] = 100;
+
+//int[] numbers = new int[3] { 100, 200, 333 };
+//int[] numbers = new int[] { 100, 200, 333 };
+//int[] numbers = { 100, 200, 333 };
+//numbers[1] = 777;
+
+//for (int i = 0; i < 3; i++)
+//{
+//    Console.WriteLine($"[{i}]: {numbers[i]}");
+//}
+
+
+
+//int[,] arr = new int[2, 3]
+//{
+//    { 1, 2, 3 },
+//    { 3, 2, 1 },
+//};
+
+//Console.WriteLine(arr[1,2]);
+
+
+
+//string[] names = new string[]
+//{
+//    "Kate",
+//    "Bob",
+//    "Ann",
+//    "John",
+//};
+
+//for (int i = 0; i < names.Length; i++)
+//{
+
+//    Console.WriteLine($"{names[i]} - {names[i].Length} symbols");
+//}
+
+
+
+//int[] numbers =
+//{
+//    'B', 'o', 'b', 7, 7, 7,
+//};
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
+Random random = new Random();
+
+for (int i = 0; i < 100; i++)
+{
+    //var randomNumber = random.Next(100);
+    //var randomNumber = random.Next(1000, 9999);
+    var randomNumber = random.Next(10);
+
+    if(randomNumber < 5)
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{randomNumber} !!!");
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine(randomNumber);
+    }
+
+    Console.ResetColor();
+}
