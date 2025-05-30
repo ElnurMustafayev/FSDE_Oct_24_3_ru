cf853474d6927e2022eb164e3c7aeed64072a54f
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-05-30 21:29:48 +0400

    Add incapsulation

---
+++ C#/IncapsulationApp/Car.cs
@@ -0,0 +1,58 @@
+﻿namespace IncapsulationApp;
+
+class Car
+{
+    public const string DEFAULT_NAME = "UNKNOWN";
+
+    private string name;
+    private int maxSpeed;
+
+    public string GetName() => this.name;
+
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Car name can not be empty!", nameof(this.name));
+        }
+        this.name = name;
+    }
+
+    public int GetMaxSpeed() => this.maxSpeed;
+
+    public void SetMaxSpeed(int maxSpeed)
+    {
+        if (maxSpeed < 0)
+        {
+            throw new ArgumentException("Max speed can not be less than 0!", nameof(this.maxSpeed));
+        }
+        this.maxSpeed = maxSpeed;
+    }
+
+    public Car()
+    {
+        this.SetName(DEFAULT_NAME);
+        this.SetMaxSpeed(default);
+    }
+
+    public Car(string name, int maxSpeed)
+    {
+        this.SetName(name);
+        this.SetMaxSpeed(maxSpeed);
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Name: {this.GetName()}");
+        Console.WriteLine($"MaxSpeed: {this.GetMaxSpeed()}");
+    }
+}
+
+//namespace TestNs
+//{
+//    class Car
+//    {
+//        public string Name;
+//        public int MaxSpeed;
+//    }
+//}
---
+++ C#/IncapsulationApp/Program.cs
@@ -0,0 +1,33 @@
+﻿//using TestNs;
+
+namespace IncapsulationApp;
+
+class Program
+{
+    public static void Main()
+    {
+        //TestNs.Car car;
+        //IncapsulationApp.Car car;
+
+
+
+        //Car car = new Car();
+
+        //car.Name = "bmw";
+        //car.MaxSpeed = 260;
+
+        //car.ShowInfo();
+
+
+
+
+
+        //Car car = new Car("Bmw", 260);
+        Car car = new Car();
+
+        //car.SetMaxSpeed(-1_000_000);
+        car.SetMaxSpeed(0);
+
+        car.ShowInfo();
+    }
+}
