8b75dae3de3c6406d54dac614e130afd0ef57039
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-06-02 20:35:38 +0400

    End Incapsulation

--- C#/IncapsulationApp/Car.cs
+++ C#/IncapsulationApp/Car.cs
@@ -7,6 +7,64 @@ class Car
     private string name;
     private int maxSpeed;
 
+
+    // Properties
+    public int MaxSpeed
+    {
+        get
+        {
+            return this.maxSpeed;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Max speed can not be less than 0!", nameof(this.maxSpeed));
+            }
+            this.maxSpeed = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Car name can not be empty!", nameof(this.name));
+            }
+            this.name = value;
+        }
+    }
+
+    public string Info
+    {
+        get
+        {
+            return $"{this.Name}: {this.MaxSpeed} km/h";
+        }
+    }
+
+    public int MyProperty
+    {
+        get
+        {
+            Console.WriteLine("GETTER!");
+            return 150;
+        }
+        set
+        {
+            Console.WriteLine($"Destination value: {value}");
+        }
+    }
+
+
+    // Accessors
+    /*
     public string GetName() => this.name;
 
     public void SetName(string name)
@@ -28,23 +86,31 @@ class Car
         }
         this.maxSpeed = maxSpeed;
     }
+    */
 
     public Car()
     {
-        this.SetName(DEFAULT_NAME);
-        this.SetMaxSpeed(default);
+        this.Name = DEFAULT_NAME;
+        this.MaxSpeed = default;
+        //this.SetName(DEFAULT_NAME);
+        //this.SetMaxSpeed(default);
     }
 
     public Car(string name, int maxSpeed)
     {
-        this.SetName(name);
-        this.SetMaxSpeed(maxSpeed);
+        this.Name = name;
+        this.MaxSpeed = maxSpeed;
+        //this.SetName(name);
+        //this.SetMaxSpeed(maxSpeed);
     }
 
     public void ShowInfo()
     {
-        Console.WriteLine($"Name: {this.GetName()}");
-        Console.WriteLine($"MaxSpeed: {this.GetMaxSpeed()}");
+        //Console.WriteLine($"Name: {this.GetName()}");
+        //Console.WriteLine($"MaxSpeed: {this.GetMaxSpeed()}");
+
+        Console.WriteLine($"{nameof(this.Name)}: {this.Name}");
+        Console.WriteLine($"{nameof(this.MaxSpeed)}: {this.MaxSpeed}");
     }
 }
 
--- C#/IncapsulationApp/Program.cs
+++ C#/IncapsulationApp/Program.cs
@@ -21,13 +21,65 @@ class Program
 
 
 
+        #region Accessors
+
+        // Type -> T
+        // field name -> Field
+
+        // void SetField(T field)
+        // T GetField();
+
+        // void SetNumber(int number);
+        // void SetText(string text);
+
+        // int GetNumber();
+        // string GetText();
+
 
         //Car car = new Car("Bmw", 260);
-        Car car = new Car();
+        //Car car = new Car();
 
         //car.SetMaxSpeed(-1_000_000);
-        car.SetMaxSpeed(0);
+        //car.SetMaxSpeed(0);
+
+        //car.ShowInfo();
+
+        #endregion
+
+        #region Properties
+
+        #region Getters
+
+        //Car car = new Car();
+        ////Console.WriteLine(car.MyProperty);
+        //int result = car.MyProperty + car.MyProperty;
+        //Console.WriteLine(car.MyProperty + car.MyProperty - car.MyProperty);
+        //car.SetMaxSpeed(car.MyProperty);
+
+        //Console.WriteLine(result);
+
+        #endregion
+
+        #region Setters
+
+        //Car car = new Car();
+
+        //car.MyProperty = 111;
+        //car.MyProperty = 222;
+        //car.MyProperty = 333;
+
+        //Console.WriteLine(car.MyProperty);
+
+        #endregion
+
+        Car car = new Car();
+
+        car.Name = "Bmw";
+        car.MaxSpeed = 280;
 
         car.ShowInfo();
+
+        #endregion
+
     }
 }
