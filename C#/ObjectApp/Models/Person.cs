bacdfcecd9c4bf15fd46a13a734c7800c60e00a5
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-06-18 20:45:15 +0400

    End Objects

---
+++ C#/ObjectApp/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace ObjectApp.Models;
+
+class Person
+{
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public override string ToString()
+    {
+        //Person newPerson = base.MemberwiseClone() as Person;
+        return $"{this.Name}: {this.BirthDate.ToShortDateString()}";
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+        //return Name.Length + this.BirthDate.Year;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is not Person)
+        {
+            return false;
+        }
+        var anotherPerson = (Person)obj;
+
+        return this.Name == anotherPerson.Name;
+    }
+}
--- C#/ObjectApp/Program.cs
+++ C#/ObjectApp/Program.cs
@@ -12,6 +12,7 @@
 
 
 
+/*
 object obj = 123;
 obj = "qwerty";
 obj = new object();
@@ -20,3 +21,44 @@ obj = true;
 obj = new int[] { 1,2,3 };
 
 Console.WriteLine(obj);
+*/
+
+using ObjectApp.Models;
+
+object obj1 = new Person()
+{
+    Name = "Bob",
+};
+
+//obj1.ToString();
+//obj1.GetType();
+//obj1.GetHashCode();
+//obj1.Equals(123);
+
+object obj2 = new Person()
+{
+    Name = "Bob",
+    BirthDate = new DateTime(1965, 10, 20),
+};
+
+Console.WriteLine(obj1.Equals(obj2));
+
+
+
+//int[] arr = new int[] { 1, 2, 3 };
+
+//object num1 = arr;
+//object num2 = arr;
+
+//Console.WriteLine(num1.GetHashCode());
+//Console.WriteLine(num2.GetHashCode());
+
+
+//Console.WriteLine(obj2);
+//Console.WriteLine(obj2.ToString());
+
+
+//Console.WriteLine(obj1.GetType());
+//Console.WriteLine(obj2.GetType());
+
+//Console.WriteLine(obj1.GetType() == obj2.GetType());
