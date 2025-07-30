0964feaa7bf1c97641b84cc6ac7e2317e30b3a79
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-07-30 21:45:08 +0400

    Add ORM

---
+++ ORM/ado.net/GettingStartedApp/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace GettingStartedApp.Entities;
+
+class Student
+{
+    public int Id { get; set; }
+    public required string Fullname { get; set; }
+    public required DateTime BirthDate { get; set; }
+    public int? CountryId { get; set; }
+    public int? GroupId { get; set; }
+
+    public override string ToString()
+    {
+        return $"Student Info:\n" +
+               $"- ID: {Id}\n" +
+               $"- Full Name: {Fullname}\n" +
+               $"- Birth Date: {BirthDate}\n" +
+               $"- Country ID: {(CountryId.HasValue ? CountryId.Value.ToString() : "N/A")}\n" +
+               $"- Group ID: {(GroupId.HasValue ? GroupId.Value.ToString() : "N/A")}";
+    }
+}
---
+++ ORM/ado.net/GettingStartedApp/Program.cs
@@ -0,0 +1,53 @@
+﻿// dotnet add package Microsoft.Data.SqlClient --version 6.1.0
+using GettingStartedApp.Entities;
+using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost;Database=ExamDb;User Id=admin;Password=admin;TrustServerCertificate=True;";
+
+/*
+SqlConnection connection = new SqlConnection(connectionString);
+
+connection.Open();
+
+var command = new SqlCommand("select count(*) from Students", connection);
+
+var result = command.ExecuteScalar();
+
+System.Console.WriteLine(result);
+*/
+
+
+List<Student> GetAllStudents()
+{
+    SqlConnection connection = new SqlConnection(connectionString);
+
+    connection.Open();
+
+    SqlCommand command = connection.CreateCommand();
+
+    command.CommandText = "select * from Students";
+
+    var reader = command.ExecuteReader();
+
+    var students = new List<Student>();
+
+    while (reader.Read())
+    {
+        var student = new Student()
+        {
+            Id = reader.GetInt32(0),
+            Fullname = reader.GetString(1),
+            BirthDate = reader.GetDateTime(2),
+            CountryId = (int?)reader.GetValue(3),
+            GroupId = (int?)reader.GetValue(4),
+        };
+
+        students.Add(student);
+    }
+
+    return students;
+}
+
+var students = GetAllStudents();
+
+students.ForEach(student => Console.WriteLine(student));
