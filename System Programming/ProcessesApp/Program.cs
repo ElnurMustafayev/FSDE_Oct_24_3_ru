fa4f1b1a2d312ee5b265044665488a1b5f6f2840
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-09-05 21:48:23 +0400

    Add Processes, Threads

---
+++ System Programming/ProcessesApp/Program.cs
@@ -0,0 +1,19 @@
+﻿//Process currentAppProcess = Process.GetCurrentProcess();
+//Console.WriteLine(currentAppProcess);
+
+//Process.Start("calc.exe");
+//Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://google.com");
+
+//var processes = Process.GetProcesses();
+
+//foreach (var process in processes)
+//{
+//    Console.WriteLine(process.StartInfo.FileName);
+//}
+
+//var calcApp = Process.GetProcesses()
+//    .First(process => process.ProcessName == "CalculatorApp");
+
+//Console.WriteLine(calcApp);
+
+//calcApp.Kill();
---
+++ System Programming/ThreadsApp/Program.cs
@@ -0,0 +1,56 @@
+﻿//Thread thread = Thread.CurrentThread;
+
+//Console.WriteLine(thread);
+
+
+//ThreadStart threadMethod = () =>
+//{
+//    Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+//    Console.WriteLine("Hello from Thread!");
+//};
+//Thread thread = new Thread(threadMethod);
+
+//thread.Start();
+//Console.WriteLine($"General Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+//Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+
+
+/*
+new Thread(() =>
+{
+    int counter = 0;
+    while (counter != 70)
+    {
+        Thread.Sleep(100);
+        Console.WriteLine($"Thread 1 {Thread.CurrentThread.ManagedThreadId} Counter: {counter++}");
+    }
+})
+{
+    IsBackground = false
+}.Start();
+
+
+var infinityThread = new Thread(() =>
+{
+    int counter = 0;
+    while (counter != 150)
+    {
+        Thread.Sleep(100);
+        Console.WriteLine($"Thread 2 {Thread.CurrentThread.ManagedThreadId} Counter: {counter++}");
+    }
+})
+{
+    IsBackground = true
+};
+
+infinityThread.Start();
+
+for (int i = 0; i < 5; i++)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine($"Main {Thread.CurrentThread.ManagedThreadId} TICK: {i}");
+}
+*/
+
+//Environment.Exit(0);
