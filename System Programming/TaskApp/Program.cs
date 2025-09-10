d533506c28ccf78249ae47bdad03eb9b0ca6908a
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-09-10 21:46:16 +0400

    Add ThreadPool, Tasks

---
+++ System Programming/TaskApp/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+var task = new Task(() =>
+{
+    while (true)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine($"Task: {Thread.CurrentThread.ManagedThreadId}");
+    }
+});
+
+task.Start();
+
+Console.ReadKey();
+*/
+
+
+/*
+Task<int> task = new Task<int>(() =>
+{
+    const int loopLimit = 5;
+    for (int i = 0; i < loopLimit; i++)
+    {
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("number loading...");
+        Console.WriteLine($"progress: {i + 1}/{loopLimit}");
+    }
+
+    return 100;
+});
+
+task.Start();
+
+var number = task.Result;
+
+Console.WriteLine($"Result: {number}");
+*/
+
+
+
+
+// Создать и запустить Task который,
+// возвращает из базы данных самого старшего пользователя
---
+++ System Programming/ThreadPoolApp/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+var tokenSource = new CancellationTokenSource();
+
+ThreadPool.QueueUserWorkItem((token) =>
+{
+    Console.WriteLine("Thread start");
+    while (token.IsCancellationRequested == false)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine(150);
+    }
+    Console.WriteLine("Thread end");
+}, tokenSource.Token, true);
+
+Console.ReadKey();
+tokenSource.Cancel();
+Console.ReadKey();
+*/
+
+
+
+/*
+ThreadPool.QueueUserWorkItem((obj) =>
+{
+    var threadId = Thread.CurrentThread.ManagedThreadId;
+
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"#1 Thread Id: {threadId}");
+        Thread.Sleep(500);
+    }
+});
+
+Console.ReadKey();
+
+ThreadPool.QueueUserWorkItem((obj) =>
+{
+    var threadId = Thread.CurrentThread.ManagedThreadId;
+
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"#2 Thread Id: {threadId}");
+        Thread.Sleep(700);
+    }
+});
+
+Console.ReadKey();
+*/
