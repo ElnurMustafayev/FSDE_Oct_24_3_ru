ca0f15f4564c81be720fff3241d47d6f171e5dd4
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-09-24 21:55:08 +0400

    Add TCP

---
+++ Network Programming/ClientServerApp/ClientApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+var tcpClient = new TcpClient();
+
+await tcpClient.ConnectAsync("localhost", 7777);
+
+var stream = tcpClient.GetStream();
+
+byte[] buffer = new byte[1000];
+
+while (true)
+{
+    int size = await stream.ReadAsync(buffer, 0, 1000);
+    
+    var messageFromServer = Encoding.ASCII.GetString(buffer, 0, size);
+    
+    System.Console.WriteLine($"Server: {messageFromServer}");
+}
---
+++ Network Programming/ClientServerApp/ServerApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string ip = "127.0.0.1";
+const int port = 7777;
+var listener = new TcpListener(IPAddress.Parse(ip), port);
+listener.Start();
+System.Console.WriteLine($"Server started '{ip}:{port}'...");
+
+while (true)
+{
+    try
+    {
+            
+        var tcpClient = await listener.AcceptTcpClientAsync();
+        System.Console.WriteLine($"New client connected: {tcpClient.Client.RemoteEndPoint}");
+
+        var stream = tcpClient.GetStream();
+
+        var messageToClient = "Hello from Server!";
+        var messageToClientInBytes = Encoding.ASCII.GetBytes(messageToClient);
+
+        _ = Task.Run(async () =>
+        {
+            while (true)
+            {
+                await stream.WriteAsync(messageToClientInBytes);
+                await stream.FlushAsync();
+
+                Thread.Sleep(1000);
+            }
+        });
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"Error: {ex}");
+    }
+}
