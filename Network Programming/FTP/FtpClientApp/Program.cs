64792fadc43a924f9860e4a3f1a227b5859cb334
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-10-01 21:43:56 +0400

    Add FTP

---
+++ Network Programming/FTP/FtpClientApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using FluentFTP;
+
+// link: ftp://ftp.uni-kl.de/pub/linux/gentoo/distfiles/1d/cat-soltys.png
+
+using FtpClient client = new FtpClient("ftp://ftp.uni-kl.de");
+
+client.Connect();
+
+client.DownloadFile(
+    localPath: "cat.png",
+    remotePath: "/pub/linux/gentoo/distfiles/1d/cat-soltys.png"
+);
+
+string[] filenames = client.GetNameListing("/pub/linux/gentoo/distfiles/1d/");
