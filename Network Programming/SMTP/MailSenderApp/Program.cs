0bb1a264c9e3e0fda1a21df6dc6dc6b1645834e8
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-10-03 20:57:44 +0400

    Add SMTP

---
+++ Network Programming/SMTP/MailSenderApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+var client = new SmtpClient(Environment.GetEnvironmentVariable("SMTP_SERVER_DOMAINNAME")!, 2525)
+{
+    Credentials = new NetworkCredential(
+        userName: Environment.GetEnvironmentVariable("SMTP_USERNAME")!,
+        password: Environment.GetEnvironmentVariable("SMTP_PASSWORD")!),
+    EnableSsl = true
+};
+
+string mailMessageHtmlBody = await File.ReadAllTextAsync("kindly_ask_professor.html");
+
+mailMessageHtmlBody = mailMessageHtmlBody
+    .Replace("{{professor_name}}", "Elnur")
+    .Replace("{{student_name}}", "Bob");
+
+var mailMessage = new MailMessage(
+    from: "bob@mycompany.com",
+    to: "elnur.mustafayeev@gmail.com"
+)
+{
+    Subject = "New student",
+    Body = mailMessageHtmlBody,
+    IsBodyHtml = true,
+};
+
+client.Send(mailMessage);
+System.Console.WriteLine("Mail sent");
