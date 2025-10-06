58b9615cefd4cfa922cfdd8051dca995332fef62
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-10-06 20:21:58 +0400

    Add HTTP

---
+++ Network Programming/HTTP/ClientApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+WebClient client = new WebClient();
+
+try {
+    var uri = new Uri("https://cdnjs.com/libraries/normalize");
+
+    var responseStr = client.DownloadString(uri);
+    
+    System.Console.WriteLine(responseStr);
+}
+catch(Exception ex) {
+    System.Console.WriteLine($"Error: {ex}");
+}
---
+++ Network Programming/HTTP/ServerApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net;
+
+HttpListener httpListener = new HttpListener();
+
+httpListener.Prefixes.Add("http://*:8080/");
+
+httpListener.Start();
